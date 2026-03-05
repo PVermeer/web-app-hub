8cdae8590f4ac9d1777dcaecdc6c1cc258a4bb2c
Pim Vermeer <pim.vermeer@gmail.com>
2026-03-05 14:07:53 +0000

    chore(ci): fix dep install

--- .github/workflows/release-deps.sh
+++ .github/workflows/release-deps.sh
@@ -2,22 +2,23 @@
 
 set -e
 
-export packages="\
-    flatpak-builder \
-    gh \
-    build-essential \
-    libglib2.0-dev \
-    libgtk-4-dev \
-    libadwaita-1-dev"
+packages=(
+    "flatpak-builder"
+    "gh"
+    "build-essential"
+    "libglib2.0-dev"
+    "libgtk-4-dev"
+    "libadwaita-1-dev"
+)
 
 if [ "$1" == "--only-export-packages" ]; then
+    export PACKAGES="${packages[*]}"
     return
 fi
 
 echo -e "\n==== Installing deps ====\n"
 
 sudo apt-get update -y
-
-sudo apt-get install -y "$packages"
+sudo apt-get install -y "${packages[@]}"
 
 echo -e "\n==== Done ====\n"
